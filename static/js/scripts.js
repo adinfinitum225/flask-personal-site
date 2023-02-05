bffee4f3c0bf3189230a2bb97c96fcbcbb51a2e4
Zach Wells <zach.13.wells@gmail.com>
2023-02-05 09:42:09 -0600

    Added change of arrow to javascript dropdown function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,8 +1,11 @@
-function dropdown() {
+function dropdown(button) {
 	var x = document.getElementById("navbar");
 	if (x.className === "clicked"){
 		x.className = "";
+		// Javascript needs Unicode characters to be referred to as follows
+		button.textContent = button.textContent.replace('\u25B2', '\u25BC');
 	} else {
 		x.className = "clicked";
+		button.textContent = button.textContent.replace('\u25BC', '\u25B2');
 	}
 }
